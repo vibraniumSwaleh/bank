60568ddd911ae6a06e0c41712ef9944ba21e323c
vibraniumSwaleh <hus.swaleh@yahoo.com>
2025-01-14 22:53:06 +0000

    test redux store

--- src/store.js
+++ src/store.js
@@ -14,13 +14,18 @@ function reducer(state = initialState, action) {
       return { ...state, balance: state.balance - action.payload };
     case 'account/requesLoan':
       if (state.loan > 0) return state;
-      return { ...state, loan: action.payload };
-    case 'account/payLaon':
+      return {
+        ...state,
+        loan: action.payload.amount,
+        balance: state.balance + action.payload.amount,
+        loanPurpose: action.payload.purpose,
+      };
+    case 'account/payLoan':
       return {
         ...state,
         loan: 0,
         loanPurpose: '',
-        balance: state.balance - action.payload,
+        balance: state.balance - state.loan,
       };
     default:
       return state;
@@ -29,5 +34,21 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: 'account/deposit', payload: 500 });
-console.log('Redux running');
+function deposit(amount) {
+  return { type: 'account/deposit', payload: amount };
+}
+function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+function requesLoan(amount, purpose) {
+  return { type: 'account/requesLoan', payload: { amount, purpose } };
+}
+function payLoan() {
+  return { type: 'account/payLoan' };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(100));
+store.dispatch(requesLoan(200, 'Buy a cheap car'));
+store.dispatch(payLoan());
+console.log(store.getState());
