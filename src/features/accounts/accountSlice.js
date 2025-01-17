2ffe51cdd7c268ba7546623e473bb13749862018
vibraniumSwaleh <hus.swaleh@yahoo.com>
2025-01-17 00:03:25 +0000

    use custom action creater in accountSlice for data fetching with api

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -13,6 +13,7 @@ const accountSlice = createSlice({
   reducers: {
     deposit(state, action) {
       state.balance += action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       state.balance -= action.payload;
@@ -31,15 +32,33 @@ const accountSlice = createSlice({
         state.loanPurpose = action.payload.purpose;
       },
     },
-    payLoan(state, action) {
+    payLoan(state) {
       state.balance -= state.loan;
       state.loan = 0;
       state.loanPurpose = '';
     },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
   },
 });
 
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export function deposit(amount, currency) {
+  if (currency === 'USD') return { type: 'account/deposit', payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: 'account/convertingCurrency' });
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`,
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+    console.log(data);
+    dispatch({ type: 'account/deposit', payload: converted });
+  };
+}
+
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
 export default accountSlice.reducer;
 
 // function reducerAccount(state = initialStateAccount, action) {
